0a053d7fb4884206ec105e3facc9f297ae2432ff
soobin <shinsoobin1111@gmail.com>
2024-01-07 00:04:05 +0900

    백준 24313 알리 수업 - 점근적 표기 1

---
+++ Baekjoon/240106/24313.py
@@ -0,0 +1,18 @@
+# 백준 24313 알고리즘 수업 - 점근적 표기 1
+
+a, b = map(int, input().split())
+c = int(input())
+n_zro = int(input())
+
+if (c-a < 0):
+    print(0)
+elif (c-a == 0):
+    if b <= 0:
+        print(1)
+    else:
+        print(0)
+else:
+    if (c-a) * n_zro - b >= 0:
+        print(1)
+    else:
+        print(0)
